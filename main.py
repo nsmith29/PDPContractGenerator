7cdb13da2ec8964dac301f5fdde77bf7bef33b40
Niamh Smith <92368623+nsmith29@users.noreply.github.com>
2025-01-23 16:24:04 +0000

    0.2.0 - Implementation of automated signing detail replacement for both the donor and recipient parties. - Slightly more code refractoring. - New Resulting contract document made with new implementations.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 
     If you have any questions or any issues with the code, please let me know :)
 """
-
+import docx.document
 import numpy as np
 import datetime
 from docx import Document
@@ -62,7 +62,8 @@ questions = {'Q1': f"{bcolors.QUESTION}What contract type would you like to crea
              'Q4': f"\n{bcolors.QUESTION}What is the recipients address?{bcolors.ACTION}"
                    f"\nPlease {bcolors.UNDERLINE}use '//' at the end of the address{bcolors.ENDC}{bcolors.ACTION} "
                    f"to signal you have finished inputting the address.{bcolors.ENDC}\n",
-             'Q5': f"\n{bcolors.QUESTION}Which country is the recipient's company in?{bcolors.ENDC}",
+             'Q5': f"\n{bcolors.QUESTION}Which country is the recipient company in?{bcolors.ENDC}",
+             'Q6': f"\n{bcolors.QUESTION}Who will be signing the contract on the recipient company's behalf?{bcolors.ENDC}",
              'QNDA1':f"\n{bcolors.QUESTION}What information does each party agree to disclose?{bcolors.ENDC}",
              'QNDA2':f"\n{bcolors.QUESTION}How long will this NDA be put in place for?{bcolors.ENDC}"}
 
@@ -80,11 +81,6 @@ class ErrMessages:
 
     @staticmethod
     def ValueErrorlist():
-        """
-            Inputs:
-                options(list) : List of the methods of data processing the user can
-                                choose from.
-        """
         text = str(f"\n{bcolors.FAIL}ERROR: {bcolors.UNDERLINE}Contract type not implemented!{bcolors.ENDC}"
                    f"{bcolors.INFO} "
                    f"\nValid implemented contract types are:{bcolors.CHOSEN}{bcolors.ITALIC} \n"
@@ -99,7 +95,7 @@ def checkinput(func):
     """
          Testing for error in user's input response to questions asked.
     """
-    def wrapper(Q, typ, ans=None, *args):
+    def wrapper(Q: str, typ: str, ans: list=None, *args):
         """
             Inputs:
                 Q(str)                  : Key for one of the questions within Core.PrePopDictsAndLists.questions.
@@ -206,6 +202,8 @@ class program:
         if self.individ_company == 'N':
             self.repcountry = ask_question('Q5', 'str')
 
+            self.representative = ask_question('Q6','str')
+
         # specific contract type questions
         self.additional = [] # empty list to populate with the user input answers related to specific contract type
         for Q in [key for key in questions.keys() if self.contype[0] in key]:
@@ -222,9 +220,9 @@ class program:
         lines = self.updatelines(document)  # [x.text for x in document.paragraphs]
 
         # search array of lines for line index for full line containing date.
-        idx4date = np.argwhere(np.asarray(lines) == 'Date: ')[0][0]
+        idx4date = self.findIndex(lines, 'Date: ')
         # add actual date to the end of this line.
-        document.paragraphs[idx4date].text = " ".join([document.paragraphs[idx4date].text, self.condate])
+        document, lines = self.linereplace(document, " ".join([document.paragraphs[idx4date].text, self.condate]), idx4date)
 
         # exchange details in document that are specific for whether contract with an individual or a company.
         if self.individ_company == 'Y':
@@ -239,7 +237,7 @@ class program:
         return newfilename
 
 
-    def forNDA(self, lines, document):
+    def forNDA(self, lines: list[str], document: docx.document.Document):
         # get line & index of "insert detail ...", replace with additional[0] & replace doc line. Update lines.
         document, lines = self.linereplace(document, *self.repvarchange('[insert details e.g. discussing the possibility'
                                                      ' of the parties entering into a joint venture]', lines,
@@ -251,7 +249,7 @@ class program:
 
         return document, lines
 
-    def forindividual(self, lines, document):
+    def forindividual(self, lines: list[str], document: docx.document.Document):
         # get line replacement for name, index of line to replace & replace line in document. Update lines.
         document, lines = self.linereplace(document, *self.repnamechange('[NAME OF INDIVIDUAL]', lines))
 
@@ -262,9 +260,13 @@ class program:
         document, lines = self.remove(document, 'OR', lines)
         document, lines = self.remove(document, '[NAME OF COMPANY]', lines)
 
+        # signature setup at end of contract
+        document, lines = self.linereplace(document, *self.PDPsignature(lines))
+        document, lines = self.linereplace(document, *self.repsignature(lines, 'individual'))
+
         return document, lines
 
-    def forcompany(self, lines,  document):
+    def forcompany(self, lines: list[str], document: docx.document.Document):
         # get line replacement for name, index of line to replace & replace line in document. Update lines.
         document, lines = self.linereplace(document, *self.repnamechange('[NAME OF COMPANY]', lines))
 
@@ -272,16 +274,38 @@ class program:
         document, lines = self.linereplace(document, *self.repaddchange('[ADDRESS]', lines))
 
         # get line replacement for '[COUNTRY]', index of line to replace & replace line in document. Update lines.
-        document, lines = self.linereplace(document, *self.repvarchange('[COUNTRY]', lines,self.repcountry))
+        document, lines = self.linereplace(document, *self.repvarchange('[COUNTRY]', lines, self.repcountry))
 
         # removal of lines which are associated with the contract being for an individual
         document, lines = self.remove(document, '[NAME OF INDIVIDUAL]', lines)
         document, lines = self.remove(document, 'OR', lines)
 
+        # signature setup at end of contract
+        document, lines = self.linereplace(document, *self.PDPsignature(lines))
+        document, lines = self.linereplace(document, *self.repsignature(lines, 'company'))
+
         return document, lines
 
+    def repsignature(self, lines, sign4):
+        # dictionary of signing detail based on repicient type.
+        signatures = {"individal": f"by {self.repname}",
+                      "company": f"on behalf of {self.repname} by its duly authorised representative, {self.representative}"}
 
-    def linereplace(self, document, varline, idx4var):
+        return self.Signatures(lines, signatures[sign4])
+
+    def PDPsignature(self, lines: list):
+
+        return self.Signatures(lines, "by James Nugent, a representative of Cosmic Audio Ltd")
+
+    def Signatures(self, lines: list, strg: str):
+        # find line containing 'Signed [by' string
+        signline, idx4sign = self.findlineandindex('Signed [by', lines)
+        # replace all but the word 'Signed' in line with strg argument
+        signline = self.repvarreplacement(signline, " ".join([x for x in signline.split() if x != "Signed"]), strg)
+
+        return signline, idx4sign
+
+    def linereplace(self, document: docx.document.Document, varline: str, idx4var: int):
         # replace line containing name in document
         document.paragraphs[idx4var].text = varline
         # update lines
@@ -290,8 +314,8 @@ class program:
         return document, lines
 
 
-    def remove(self, document, strg, lines):
-        # find line contain strg wanting to be removed
+    def remove(self, document: docx.document.Document, strg: str, lines: list):
+        # find line containing strg wanting to be removed
         line, inx2remove = self.findlineandindex(strg, lines)
         # clear the paragraph line within document
         document.paragraphs[inx2remove].clear()
@@ -301,32 +325,32 @@ class program:
         return document, lines
 
 
-    def updatelines(self, document):
+    def updatelines(self, document: docx.document.Document):
 
         return [x.text for x in document.paragraphs]
 
 
-    def repnamechange(self, strg, lines):
+    def repnamechange(self, strg: str, lines: list):
 
         return self.repvarchange(strg, lines, self.repname)
 
 
-    def repvarchange(self, strg, lines, var):
+    def repvarchange(self, strg: str, lines: list, var: str):
         varlines, idx4lines = self.findlineandindex(strg, lines)
         varlines = self.repvarreplacement(varlines, strg, var)
 
         return varlines, idx4lines
 
 
-    def repaddchange(self, strg, lines):
+    def repaddchange(self, strg: str, lines: list):
         # find line containing strg and index of line.
         addline, idx4add = self.findlineandindex(strg, lines)
         # replace strg within line with repadd.
-        if len(self.repadd) > 1:
+        if len(self.repadd) > 1: # if user input for address was multiple lines
             if "," in self.repadd[0]:
-                self.repadd = " ".join(self.repadd)
+                self.repadd = " ".join(self.repadd)# if commas already put at end of each address line
             else:
-                self.repadd = ", ".join(self.repadd)
+                self.repadd = ", ".join(self.repadd) # if commas not put at end of each address line
         else:
             self.repadd = self.repadd[0]
         addline = self.repvarreplacement(addline, strg, self.repadd)
@@ -334,21 +358,23 @@ class program:
         return addline, idx4add
 
 
-    def repvarreplacement(self, varline, strg, var):
+    def repvarreplacement(self, varline: str, strg: str, var: str):
 
         # replace strg within line with var.
         return varline.replace(strg, var)
 
 
-    def findlineandindex(self, strg, lines):
+    def findlineandindex(self, strg: str, lines: list):
         # find total line containing strg in lines.
         line = [x for x in lines if strg in x][0]
         # identify index of this line.
-        idx4add = np.argwhere(np.asarray(lines) == line)[0][0]
+        idx4add = self.findIndex(lines, line)
 
         return line, idx4add
 
+    def findIndex(self, lines: list, line: str):
 
+        return np.argwhere(np.asarray(lines) == line)[0][0]
 
 if __name__ =='__main__':
     program()
